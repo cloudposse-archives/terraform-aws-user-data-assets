61988f328858ecf91bf83f987b0d39775a27d2c2
Igor Rodionov <goruha@gmail.com>
2017-08-10 00:15:12 +0600

    EC2 user data to work with assets (#1)
    * Added assets dir
    
    * Added fix of name conflicts
    
    * Fix assets fix
    
    * Fix assets fix
    
    * Fix assets fix
    
    * Fix assets fix
    
    * Fix assets ops
    
    * Fix assets ops
    
    * Added PR comments

---
+++ main.tf
@@ -0,0 +1,66 @@
+# Define composite variables for resources
+module "label" {
+  source    = "git::https://github.com/cloudposse/tf_label.git?ref=tags/0.1.0"
+  namespace = "${var.namespace}"
+  name      = "${var.name}"
+  stage     = "${var.stage}"
+}
+
+data "template_file" "default" {
+  template = "${file("${path.module}/user_data.sh")}"
+
+  vars {
+    name              = "${var.name}"
+    dir               = "${var.dir}"
+    bucket            = "${var.bucket}"
+    backup_enabled    = "${var.backup_enabled}"
+    backup_frequency  = "${var.backup_frequency}"
+    ssh_user          = "${var.ssh_user}"
+  }
+}
+
+## IAM Role Policy that allows access to S3
+resource "aws_iam_policy" "default" {
+  name = "${module.label.id}-assets"
+
+  lifecycle {
+    create_before_destroy = true
+  }
+
+  policy = "${data.aws_iam_policy_document.default.json}"
+}
+
+data "aws_iam_policy_document" "default" {
+  statement {
+    actions = [
+      "s3:ListAllMyBuckets",
+    ]
+
+    effect = "Allow"
+
+    resources = [
+      "arn:aws:s3:::*",
+    ]
+  }
+
+  statement {
+    actions = [ "s3:ListBucket" ]
+
+    effect = "Allow"
+
+    resources = [
+      "${format("arn:aws:s3:::%s", replace(var.bucket, "/\\/[^|]*/", ""))}"
+    ]
+  }
+
+  statement {
+    actions = [ "s3:*" ]
+
+    effect = "Allow"
+
+    resources = [
+      "${format("arn:aws:s3:::%s", replace(var.bucket, "/\\/[^|]*/", ""))}",
+      "${format("arn:aws:s3:::%s/*", replace(var.bucket, "/\\/[^|]*/", ""))}"
+    ]
+  }
+}
