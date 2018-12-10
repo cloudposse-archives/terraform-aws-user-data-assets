8189d908f9cb3fa9cca58c37e21ccce84dd96245
solairerove <solairerove@gmail.com>
2018-12-10 15:17:20 +0300

    doc: terraform format outputs file

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,9 @@
 output "user_data" {
-  value = "${data.template_file.default.rendered}"
+  value       = "${data.template_file.default.rendered}"
   description = "description here"
 }
 
 output "policy_arn" {
-  value = "${aws_iam_policy.default.arn}"
+  value       = "${aws_iam_policy.default.arn}"
   description = "description here"
 }
